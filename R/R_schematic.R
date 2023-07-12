81234bd10c6f0029f5150813c215cfe0afcf722e
Anne Cori <a.cori@imperial.ac.uk>
2023-07-12 17:44:34 +0100

    adding effect of underreporting

--- R/R_schematic.R
+++ R/R_schematic.R
@@ -9,14 +9,14 @@ source("R/functions.R")
 par(las = 0, xaxs="i", yaxs="i", mar = c(7, 7, 2, 2))
 
 epsilon <- 0.1
-r <- 0.2
+r <- 0.25
 tt <- seq(0, 8, 0.01)
 xmax <- max(tt)
 ymax <- max(exp(r*tt))
 
 
 ################################################################################
-### R illustration, panel 1
+### R illustration, baseline panel
 ################################################################################
 
 ## exponential growth curve
@@ -29,26 +29,84 @@ text(0.9*xmax, 0.95*ymax, expression(I[t]~"="~I[0]~e^rt))
 #### little men ####
 
 ## primary case
-col <- "grey"
-lwd <- 2
-draw_man(x0 = 0, y0 = 0, radius = 0.2, col = col, lwd = lwd)
+col <- "black"
+lwd <- 3
+radius <- 0.2
+draw_man(x0 = 0, y0 = 0, radius = radius, col = col, lwd = lwd)
 
 ## secondary cases
-gt <- 5.4
-draw_man(x0 = gt, y0 = 0, radius = 0.2, col = col, lwd = lwd)
-draw_man(x0 = gt, y0 = 1, radius = 0.2, col = col, lwd = lwd)
-draw_man(x0 = gt, y0 = 2, radius = 0.2, col = col, lwd = lwd)
-
+gt <- 5.45
+R <- 4
+for(i in seq(0, R-1))
+{
+  draw_man(x0 = gt, y0 = i, radius = radius, col = col, lwd = lwd)
+}
 #### generation time ####
 par(xpd = TRUE)
-segments(0, -1, gt, -1, lty = 2)
-segments(gt - radius, -1 - 0.5*radius, gt, -1)
-segments(gt - radius, -1 + 0.5*radius, gt, -1)
-text(gt/2, -1.25, "GT")
+ygt <- -.8
+segments(0, ygt, gt, ygt, lty = 2)
+segments(gt - radius, ygt - 0.5*radius, gt, ygt)
+segments(gt - radius, ygt + 0.5*radius, gt, ygt)
+text(gt/2, ygt-.3, "GT")
 par(xpd = FALSE)
 
 #### Reproduction number ####
-iArrows(radius, 1+epsilon, gt + radius, 3+epsilon,
+iArrows(radius, 1+epsilon, gt + radius, R+epsilon,
         h.lwd=1, sh.lwd=1, sh.col="black", h.lty = 1, sh.lty = 2,
         curve=0.75, width=1.75, size=1.25)
-text(gt/2, 4, "x R")
+text(gt/2, R+.5, "x R")
+
+################################################################################
+### R illustration, under-reporting
+################################################################################
+
+p_report <- 0.5
+
+## exponential growth curve
+plot(tt, exp(r*tt), type = "l", xlab = "", ylab = "", bty = "n",
+     xlim = range(tt), ylim = c(0, 1.1*ymax), col = "grey",axes = FALSE)
+axis(side = 1)
+yaxis <- seq(0, 16, 2)
+axis(side = 2, at = yaxis, labels = yaxis*2)
+lines(tt, exp(r*tt)*p_report)
+mtext("Time", 1, 5)
+mtext("Incidence", 2, 3)
+text(0.9*xmax, 0.95*ymax, expression(I[0]~e^rt))
+text(0.9*xmax, 0.95*ymax*p_report, expression(rho~I[0]~e^rt))
+
+#### little men ####
+
+## primary case
+col <- "black"
+col2 <- "grey"
+lwd <- 3
+radius <- 0.1
+draw_man(x0 = 0, y0 = 0, radius = radius, height = .5, col = col, lwd = lwd)
+draw_man(x0 = 0, y0 = .5, radius = radius, height = .5, col = col2, lwd = lwd)
+
+## secondary cases
+gt <- 5.5
+R <- 4
+for(i in seq(0, R-1))
+{
+  draw_man(x0 = gt, y0 = i/2, radius = radius, height = .5, col = col, lwd = lwd)
+}
+for(i in seq(R, 2*R-1))
+{
+  draw_man(x0 = gt, y0 = i/2, radius = radius, height = .5, col = col2, lwd = lwd)
+}
+#### generation time ####
+par(xpd = TRUE)
+ygt <- -.8
+segments(0, ygt, gt, ygt, lty = 2)
+segments(gt - radius, ygt - 0.5*radius, gt, ygt)
+segments(gt - radius, ygt + 0.5*radius, gt, ygt)
+text(gt/2, ygt-.3, "GT")
+par(xpd = FALSE)
+
+#### Reproduction number ####
+iArrows(radius, 1+epsilon, gt + radius, R+epsilon,
+        h.lwd=1, sh.lwd=1, sh.col="black", h.lty = 1, sh.lty = 2,
+        curve=0.75, width=1.75, size=1.25)
+text(gt/2, R+.5, "x R")
+
--- R/functions.R
+++ R/functions.R
@@ -1,9 +1,9 @@
 ## little man function
 
-draw_man <- function(x0 = 0, y0 = 0, radius = 0.2, col = "grey", lwd = 2)
+draw_man <- function(x0 = 0, y0 = 0, radius = 0.2, height = 1, col = "grey", lwd = 2)
 {
-  draw.circle(x = x0+radius, y = y0+1-radius, radius = radius, border = col, lwd = lwd)
-  segments(x0+radius, y0+radius, x0+radius, y0+1-2*radius, col = col, lwd = lwd)
+  draw.circle(x = x0+radius, y = y0+height-radius, radius = radius, border = col, lwd = lwd)
+  segments(x0+radius, y0+radius, x0+radius, y0+height-2*radius, col = col, lwd = lwd)
   segments(x0, y0, x0+radius, y0+radius, col = col, lwd = lwd)
   segments(x0+2*radius, y0, x0+radius, y0+radius, col = col, lwd = lwd)
   segments(x0, y0+1.5*radius, x0+radius, y0+2.5*radius, col = col, lwd = lwd)
